ac1d9644ed10eaba0292deeda7ada003809d9ca4
aylinyesilt <108435979+aylinyesilt@users.noreply.github.com>
2022-06-30 23:56:33 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,4 @@
+select * from country where country like 'A%a' ;
+select * from country where country like '%_____n' ;
+select title from film where title ILIKE 'T%T%T%%T' ;
+Select * from film where title like 'C%' and length > 90 and rental_rate = 2.99 ;
